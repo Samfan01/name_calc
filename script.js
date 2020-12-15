65c4708ee7a7499e1b805323ac248c7e2a975ca8
samfan01 <dsamfan@gmail.com>
2020-12-15 17:53:25 +0300

    added js funcions

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-
-var dd  = document.getElementById("date").value;
-
-
-//var mm=parseInt(document.getElementById("month").value);
-//var yy=parseInt(document.getElementById("year").value);
-//var cc=parseInt(document.getElementById("century").value);
-
-
+var femaleNames=[' Akosua','Adwoa','Abenaa','Akua','Yaa','Afua',' Ama']
+var maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku',' Yaw','Kofi','Kwame']
 
 function some(){
 
-   alert(dd);}
+var dd = parseInt(document.getElementById("date").value);
+var mm=parseInt(document.getElementById("month").value);
+var yy=parseInt(document.getElementById("year").value);
+var cc=parseInt(document.getElementById("century").value);
+var gender=document.getElementById('sel1').value;
+var day =parseInt( ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7)
+
+if(yy<=0 || yy=="" || yy>20){
+    alert('invalid year')
+}
+if(mm<=0 || mm=="" || mm>12){
+    alert('invalid month')
+}
+if(dd<=0 || dd=="" || dd>31){
+    alert('invalid day')
+}
+if(cc<=0 || cc=="" || cc>20){
+    alert('invalid cent')
+}//else if(0<yy<20 && 0<mm<12 && 0<dd<31 && 0<cc<20 ){
+    //alert(day)
+}
+if(gender==="male" ){
+    alert(maleNames[day])
+}
+///if(gender===sel1){
+    //alert(day)
+//}
+
+   
+}

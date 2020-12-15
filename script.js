e321b378f28f8d7ac8ef82288e096bc055bf30bd
samfan01 <dsamfan@gmail.com>
2020-12-15 15:39:50 +0300

    added readme

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function output(){
-var input=parseInt(document.getElementById("birth").value);
-var input1=parseInt(document.getElementById("birth1").value);
-var input2=parseInt(document.getElementById("birth2").value);
-//var split=input2.charAt(0);
-//var split1=input2.charAt(1);
-//var join=split+split1;
-//var split2=input2.charAt(2);
-//var split3=input2.charAt(3);
-//var join1=split2+split3;
-//var gender=sel1
 
-//var day="( ( (join/4) -2*join-1) + ((5*join1/4) ) + ((26*(input1+1)/10)) + input )%7
+var dd  = document.getElementById("date").value;
+
+
+//var mm=parseInt(document.getElementById("month").value);
+//var yy=parseInt(document.getElementById("year").value);
+//var cc=parseInt(document.getElementById("century").value);
+
+
+
 function some(){
-    alert(input1);
-}
-}
+
+   alert(dd);}

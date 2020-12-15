86e83a4f4d17e1e19bf7369a6128b68ef386bf7c
samfan01 <dsamfan@gmail.com>
2020-12-15 08:45:48 +0300

    added readme and liscence files

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-var age=prompt("What is your age?")
+var input=document.getElementById("birth").value;
+var input1=document.getElementById("birth1").value;
+var input2=document.getElementById("birth2").value;
+//var split=input2.charAt(0);
+//var split1=input2.charAt(1);
+//var join=split+split1;
+//var split2=input2.charAt(2);
+//var split3=input2.charAt(3);
+//var join1=split2+split3;
+//var gender=sel1
+
+//var day="( ( (join/4) -2*join-1) + ((5*join1/4) ) + ((26*(input1+1)/10)) + input )%7
+function some(){
+    alert(input1);
+}
